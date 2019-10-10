23bd4094d65c7de88e1dc89707e8a75540a35622
James Couch <James.Couch@allambicare.org.au>
2019-10-11 08:46:45 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using System;
+
+namespace testGraphQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var schema = Schema.For(@"
+              type Query {
+                hello: String
+              }
+
+                type Query {
+                    
+                }
+            ");
+
+            var root = new { Hello = "Hello World!" };
+            var json = schema.Execute(_ =>
+            {
+                _.Query = "{ hello }";
+                _.Root = root;
+            });
+
+            Console.WriteLine(json);
+        }
+    }
+}
